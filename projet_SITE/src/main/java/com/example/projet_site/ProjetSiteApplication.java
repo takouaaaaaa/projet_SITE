2fd65dde05e300cfed70219c84011104be38fe08
takouaaaaaa <takouabennasr30@gmail.com>
2025-06-09 22:34:21 +0100

    first commit

---
+++ projet_SITE/src/main/java/com/example/projet_site/ProjetSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSiteApplication.class, args);
+    }
+
+}
